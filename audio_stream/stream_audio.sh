17565f92b05ee87010add0f26675c3906273e0ab
Nolan Newman <123785135+nolannewman@users.noreply.github.com>
2024-11-14 13:38:07 -0700

    Update stream_audio.sh
    Added "-stream_loop -1" flag to ensure each audio file loops until the script is manually exited by the user.

--- audio_stream/stream_audio.sh
+++ audio_stream/stream_audio.sh
@@ -24,16 +24,16 @@ PORT9=5017
 PORT10=5019
 
 # Stream each audio file using RTP with 16-bit PCM to a unique address and port
-ffmpeg -re -i "bread.mp3" -c:a pcm_s16le -ar 48000 -ac 1 -f rtp rtp://$MULTICAST_IP1:$PORT1 &
-ffmpeg -re -i "hip-hop.mp3" -c:a pcm_s16le -ar 48000 -ac 1 -f rtp rtp://$MULTICAST_IP2:$PORT2 &
-ffmpeg -re -i "firmament.mp3" -c:a pcm_s16le -ar 48000 -ac 1 -f rtp rtp://$MULTICAST_IP3:$PORT3 &
-ffmpeg -re -i "cars.mp3" -c:a pcm_s16le -ar 48000 -ac 1 -f rtp rtp://$MULTICAST_IP4:$PORT4 &
-ffmpeg -re -i "helicopter.mp3" -c:a pcm_s16le -ar 48000 -ac 1 -f rtp rtp://$MULTICAST_IP5:$PORT5 &
-ffmpeg -re -i "drum.mp3" -c:a pcm_s16le -ar 48000 -ac 1 -f rtp rtp://$MULTICAST_IP6:$PORT6 &
-ffmpeg -re -i "rain.mp3" -c:a pcm_s16le -ar 48000 -ac 1 -f rtp rtp://$MULTICAST_IP7:$PORT7 &
-ffmpeg -re -i "hawk.mp3" -c:a pcm_s16le -ar 48000 -ac 1 -f rtp rtp://$MULTICAST_IP8:$PORT8 &
-ffmpeg -re -i "clock.mp3" -c:a pcm_s16le -ar 48000 -ac 1 -f rtp rtp://$MULTICAST_IP9:$PORT9 &
-ffmpeg -re -i "river.mp3" -c:a pcm_s16le -ar 48000 -ac 1 -f rtp rtp://$MULTICAST_IP10:$PORT10 &
+ffmpeg -stream_loop -1 -re -i "bread.mp3" -c:a pcm_s16le -ar 48000 -ac 1 -f rtp rtp://$MULTICAST_IP1:$PORT1 &
+ffmpeg -stream_loop -1 -re -i "hip-hop.mp3" -c:a pcm_s16le -ar 48000 -ac 1 -f rtp rtp://$MULTICAST_IP2:$PORT2 &
+ffmpeg -stream_loop -1 -re -i "firmament.mp3" -c:a pcm_s16le -ar 48000 -ac 1 -f rtp rtp://$MULTICAST_IP3:$PORT3 &
+ffmpeg -stream_loop -1 -re -i "cars.mp3" -c:a pcm_s16le -ar 48000 -ac 1 -f rtp rtp://$MULTICAST_IP4:$PORT4 &
+ffmpeg -stream_loop -1 -re -i "helicopter.mp3" -c:a pcm_s16le -ar 48000 -ac 1 -f rtp rtp://$MULTICAST_IP5:$PORT5 &
+ffmpeg -stream_loop -1 -re -i "drum.mp3" -c:a pcm_s16le -ar 48000 -ac 1 -f rtp rtp://$MULTICAST_IP6:$PORT6 &
+ffmpeg -stream_loop -1 -re -i "rain.mp3" -c:a pcm_s16le -ar 48000 -ac 1 -f rtp rtp://$MULTICAST_IP7:$PORT7 &
+ffmpeg -stream_loop -1 -re -i "hawk.mp3" -c:a pcm_s16le -ar 48000 -ac 1 -f rtp rtp://$MULTICAST_IP8:$PORT8 &
+ffmpeg -stream_loop -1 -re -i "clock.mp3" -c:a pcm_s16le -ar 48000 -ac 1 -f rtp rtp://$MULTICAST_IP9:$PORT9 &
+ffmpeg -stream_loop -1 -re -i "river.mp3" -c:a pcm_s16le -ar 48000 -ac 1 -f rtp rtp://$MULTICAST_IP10:$PORT10 &
 
 # Wait for all background processes to finish
 wait
