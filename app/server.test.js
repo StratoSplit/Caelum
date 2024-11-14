134b824a2febff0f9f405ecd9b6e7e8f61a5a865
Dallon Jarman <dmj296@nau.edu>
2024-11-14 10:11:58 -0700

    Writing up a simple test case

---
+++ app/server.test.js
@@ -0,0 +1,24 @@
+const request = require('supertest');
+const http = require('http');
+const express = require('express');
+
+const app = express();
+app.get('/', (req, res) => res.status(200).send('OK'));
+
+describe('GET /', () => {
+    let server;
+
+    beforeAll((done) => {
+        server = http.createServer(app);
+        server.listen(8000, done);
+    });
+
+    afterAll((done) => {
+        server.close(done);
+    });
+
+    it('should return 200 OK', async () => {
+        const response = await request(server).get('/');
+        expect(response.status).toBe(200);
+    });
+});
