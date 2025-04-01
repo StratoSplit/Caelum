5ba5626404d29fa1ee91dc6bb0fd8a2f487ed181
Nolan Newman <nn377@nau.edu>
2025-04-01 11:13:48 -0700

    Fixed corrupt data continuing to play

--- app/public/audio-processor.js
+++ app/public/audio-processor.js
@@ -4,6 +4,7 @@ class PCMProcessor extends AudioWorkletProcessor {
     this.buffer = new Float32Array(4096);
     this.readIndex = 0;
     this.writeIndex = 0;
+    this.dataLength = 0; // Track valid data in buffer
 
     this.port.onmessage = (event) => {
       const newData = event.data;
@@ -12,22 +13,33 @@ class PCMProcessor extends AudioWorkletProcessor {
   }
 
   writeData(newData) {
-    // Write incoming data to the buffer, wrapping around as necessary
     for (let i = 0; i < newData.length; i++) {
       this.buffer[this.writeIndex] = newData[i];
       this.writeIndex = (this.writeIndex + 1) % this.buffer.length;
+
+      // Avoid overwriting unread data
+      if (this.dataLength < this.buffer.length) {
+        this.dataLength++;
+      } else {
+        // Overwrite oldest data by advancing readIndex
+        this.readIndex = (this.readIndex + 1) % this.buffer.length;
+      }
     }
   }
 
   process(inputs, outputs) {
     const output = outputs[0][0];
     for (let i = 0; i < output.length; i++) {
-      output[i] = this.buffer[this.readIndex];
-      this.readIndex = (this.readIndex + 1) % this.buffer.length;
+      if (this.dataLength > 0) {
+        output[i] = this.buffer[this.readIndex];
+        this.readIndex = (this.readIndex + 1) % this.buffer.length;
+        this.dataLength--;
+      } else {
+        output[i] = 0; // Silence if buffer is empty
+      }
     }
     return true;
   }
 }
 
 registerProcessor('pcm-processor', PCMProcessor);
-
